0e085035193e78d46f05ab0fe810cdadefff097c
Tria Software <desenvolvimento@triasoftware.com.br>
2020-01-06 09:04:42 -0200

    Update: adicionado barras

--- lib/brcobranca/boleto/template/rghost_proposta.rb
+++ lib/brcobranca/boleto/template/rghost_proposta.rb
@@ -136,7 +136,7 @@ module Brcobranca
 
           # cedente
           doc.moveto x: colunas[0], y: linhas[2]
-          doc.show "#{boleto.cedente} - #{boleto.documento_cedente} - #{boleto.cedente_endereco}"
+          doc.show "#{boleto.cedente} / #{boleto.documento_cedente} / #{boleto.cedente_endereco}"
 
           # vencimento
           doc.moveto x: colunas[7], y: linhas[2]
